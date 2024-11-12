1299519d78c44de41aac370e0c56adfa81a8fa9a
anime lover <animelovera1435@gmail.com>
2024-11-12 03:46:04 -0800

    hood sening done

---
+++ www/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $('.text').textillate({
+        loop: true,
+        sync: true,
+        in:{
+            effect: "bouncein",
+        },
+        out:{
+            effect: "bounceout",
+        },
+    });
+});
